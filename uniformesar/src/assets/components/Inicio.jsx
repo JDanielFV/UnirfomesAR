9cfe795b82463a61965357c40b2bab6a19e547aa
J. Daniel Flores Vega <119628732+JDanielFV@users.noreply.github.com>
2024-11-17 15:55:32 -0600

    Prueba de actualizacion online

--- uniformesar/src/assets/components/Inicio.jsx
+++ uniformesar/src/assets/components/Inicio.jsx
@@ -1,14 +1,22 @@
 import styled from 'styled-components'
 
 
+const SectionBackground = styled.div`
+background: url('./fondo.jpg  ');
+`
 const InicioContainer = styled.div`
 background: url('./fondo.jpg  ');
-
+`
+const LogoContainer = styled.div`
+background: url('./fondo.jpg  ');
+`
+const TextContainer = styled.div`
+background: url('./fondo.jpg  ');
 `
 
 const Welcome = () => {
   return (
-    <>
+    <SectionBackground>
       <InicioContainer>
         <div>
           <img src='./icono.svg'></img>
@@ -18,7 +26,7 @@ const Welcome = () => {
           <p>Con Más de 10 años de experiencia En confección y diseño de uniformes. Creamos la marca Uniformes A&R, que más que un uniforme de trabajo es ropa cómoda confortable resistente y con un diseño innovador por lo que estamos comprometidos contigo. Tenemos el compromiso de brindarte la mejor imagen de trabajo y casual que tu necesitas, a través de sus uniformes.</p>
         </div>
       </InicioContainer>
-    </>
+    </SectionBackground>
   )
 }
 
